ecbe43729689211f452745f9c126caa953d1b86d
Maria Eduarda Barone <duda.barone@outlook.com>
2024-10-21 16:57:32 +0200

    -Method getListAuthor and getListAuthorId in MagazineService

--- src/main/java/com/library/course/mapper/MagazineMapper.java
+++ src/main/java/com/library/course/mapper/MagazineMapper.java
@@ -3,35 +3,24 @@ package com.library.course.mapper;
 import com.library.course.entity.Author;
 import com.library.course.entity.Magazine;
 import com.library.course.model.CreateMagazineDTO;
-import com.library.course.service.AuthorService;
+import com.library.course.service.MagazineService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MagazineMapper {
     @Autowired
-    AuthorMapper authorMapper;
-    @Autowired
-    AuthorService authorService;
+    MagazineService magazineService;
 
     public Magazine createMagazineDTOToMagazine(CreateMagazineDTO createMagazineDTO){
-        List<Long> authorIdList = createMagazineDTO.getAuthorIdList();
-        List<Author> authorList = new ArrayList<>();
-        authorIdList.forEach(a-> {
-            try {
-                authorList.add(authorMapper.fromDTOtoAuthor(authorService.getAuthorById(a)));
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        });
+        List<Author> authorList = magazineService.getListAuthor(createMagazineDTO);
+
         return Magazine.builder().numberMagazine(createMagazineDTO.getNumberMagazine()).tipologyMagazine(createMagazineDTO.getTipologyMagazine()).publishedDate(createMagazineDTO.getPublishedDate()).title(createMagazineDTO.getTitle()).authorList(authorList).build();
     }
 
     public CreateMagazineDTO magazineToCreateMagazineDTO(Magazine magazine){
-        List<Author> authorList = magazine.getAuthorList();
-        List<Long> authorIdList = new ArrayList<>();
-        authorList.forEach(a->authorIdList.add(a.getId()));
+        List<Long> authorIdList = magazineService.getListAuthorId(magazine);
+
         return CreateMagazineDTO.builder().numberMagazine(magazine.getNumberMagazine()).tipologyMagazine(magazine.getTipologyMagazine()).publishedDate(magazine.getPublishedDate()).title(magazine.getTitle()).authorIdList(authorIdList).build();
     }
 
--- src/main/java/com/library/course/service/MagazineService.java
+++ src/main/java/com/library/course/service/MagazineService.java
@@ -1,5 +1,6 @@
 package com.library.course.service;
 
+import com.library.course.entity.Author;
 import com.library.course.entity.Magazine;
 import com.library.course.mapper.AuthorMapper;
 import com.library.course.mapper.MagazineMapper;
@@ -7,6 +8,9 @@ import com.library.course.model.CreateMagazineDTO;
 import com.library.course.repository.MagazineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MagazineService {
@@ -19,9 +23,28 @@ public class MagazineService {
     @Autowired
     AuthorMapper authorMapper;
 
-    public CreateMagazineDTO addMagazine(CreateMagazineDTO createMagazineDTO){
+    public CreateMagazineDTO addMagazine(CreateMagazineDTO createMagazineDTO) {
         Magazine magazine = magazineMapper.createMagazineDTOToMagazine(createMagazineDTO);
         return magazineMapper.magazineToCreateMagazineDTO(magazineRepository.save(magazine));
     }
 
+    public List<Author> getListAuthor(CreateMagazineDTO createMagazineDTO) {
+        List<Long> authorIdList = createMagazineDTO.getAuthorIdList();
+        List<Author> authorList = new ArrayList<>();
+        authorIdList.forEach(a -> {
+            try {
+                authorList.add(authorMapper.fromDTOtoAuthor(authorService.getAuthorById(a)));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return authorList;
+    }
+
+    public List<Long> getListAuthorId(Magazine magazine){
+        List<Author> authorList = magazine.getAuthorList();
+        List<Long> authorIdList = new ArrayList<>();
+        authorList.forEach(a->authorIdList.add(a.getId()));
+        return authorIdList;
+    }
 }
