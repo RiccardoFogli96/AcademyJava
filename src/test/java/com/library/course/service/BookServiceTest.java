ef450e03da4a82d0279a2df6dc3621283315f870
Federica Parillo <federicaparillo.94@gmail.com>
2024-10-30 17:53:09 +0100

    Added some test

--- src/test/java/com/library/course/service/BookServiceTest.java
+++ src/test/java/com/library/course/service/BookServiceTest.java
@@ -4,9 +4,7 @@ import com.library.course.entity.Author;
 import com.library.course.entity.Book;
 import com.library.course.mapper.AuthorMapper;
 import com.library.course.mapper.BookMapper;
-import com.library.course.model.AuthorDTO;
-import com.library.course.model.BookDTO;
-import com.library.course.model.CreateBookDTO;
+import com.library.course.model.*;
 import com.library.course.repository.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,7 +13,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,56 +36,99 @@ class BookServiceTest {
     @Mock
     private AuthorMapper authorMapper;
 
+
     @InjectMocks
     private BookService bookService;
 
     private CreateBookDTO createBookDTO;
+    private CreateBookDTO createBookDTO2;
     private AuthorDTO authorDTO;
     private Book book;
+    private Book book2;
+    private Book changedBook;
+    private BookDTO changedBookDTO;
     private BookDTO bookDTO;
+    private BookDTO bookDTO2;
     private Author author;
+    private Author author2;
+    private List<Book> books;
+    private ModifyBookDTO modifyBookDTO;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
 
         author = Author.builder()
                 .id(1L)
                 .build();
 
-       createBookDTO = CreateBookDTO.builder()
+        createBookDTO = CreateBookDTO.builder()
                 .titolo("IT")
                 .authorId(1L)
                 .build();
 
-       authorDTO = AuthorDTO.builder()
-               .id(1L)
-               .build();
+        createBookDTO2 = CreateBookDTO.builder()
+                .authorId(1L)
+                .titolo("IT")
+                .build();
 
-       book = Book.builder()
-               .titolo("IT")
-               .author(author)
-               .build();
+        authorDTO = AuthorDTO.builder()
+                .id(1L)
+                .build();
+        author2 = Author.builder()
+                .id(2L)
+                .build();
 
-       bookDTO = BookDTO.builder()
-               .titolo("IT")
-               .authorId(1L)
-               .id(1L)
-               .build();
+        book = Book.builder()
+                .titolo("IT")
+                .id(1L)
+                .author(author)
+                .build();
+
+        book2 = Book.builder()
+                .titolo("La guerra dei papaveri")
+                .id(2L)
+                .author(author2)
+                .build();
+
+        bookDTO = BookDTO.builder()
+                .titolo("IT")
+                .authorId(1L)
+                .id(1L)
+                .build();
+
+        bookDTO2 = BookDTO.builder()
+                .titolo("La guerra dei papaveri")
+                .authorId(2L)
+                .id(2L)
+                .build();
+
+        books = asList(book, book2);
+
+        modifyBookDTO = new ModifyBookDTO(1L, "Il nome della rosa");
+        changedBook = Book.builder()
+                .titolo("Il nome della rosa")
+                .id(1L)
+                .author(author)
+                .build();
+
+        changedBookDTO = BookDTO.builder()
+                .titolo("Il nome della rosa")
+                .id(1L)
+                .authorId(1L)
+                .build();
     }
 
 
-
     @Test
-    //addBook testing
     void addBook_ShouldThrowException_WhenAuthorNotFound() throws Exception {
         String error = "Author not exist";
         when(authorService.getAuthorDTOById(createBookDTO.getAuthorId())).thenThrow(new Exception(error));
-         Exception e = assertThrows(Exception.class,()-> bookService.addBook(createBookDTO));
+        Exception e = assertThrows(Exception.class, () -> bookService.addBook(createBookDTO));
         assertEquals(error, e.getMessage());
     }
 
     @Test
-    void addBook_ShouldThrowException_WhenTitleAndAuthorIdAlreadyExist()throws Exception{
+    void addBook_ShouldThrowException_WhenTitleAndAuthorIdAlreadyExist() throws Exception {
 
         when(authorService.getAuthorDTOById(createBookDTO.getAuthorId())).thenReturn(authorDTO);
         when(authorMapper.fromDTOtoAuthor(authorDTO)).thenReturn(author);
@@ -93,7 +139,7 @@ class BookServiceTest {
     }
 
     @Test
-    void addBook_ShouldReturnBookDTO_WhenItsAllOk()throws Exception{
+    void addBook_ShouldReturnBookDTO_WhenItsAllOk() throws Exception {
         Book newBook = Book.builder()
                 .id(1L)
                 .titolo("IT")
@@ -114,30 +160,96 @@ class BookServiceTest {
     }
 
     @Test
-    void getBookDTOById() {
+    void getBookDTOById_WhenIdIsNotPresent_ShouldThrowException() {
+        when(bookRepository.findById(book.getId())).thenReturn(Optional.empty());
+        assertThrows(Exception.class, () -> bookService.getBookDTOById(book.getId()));
+        Exception e = assertThrows(Exception.class, () -> bookService.getBookDTOById(anyLong()));
+        assertEquals("Book not found", e.getMessage());
     }
 
     @Test
-    void getBookById() {
+    void getBookDTOById_WheinItsAllOk() throws Exception {
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.of(book));
+        when(bookMapper.fromBookToDTO(book)).thenReturn(bookDTO);
+        BookDTO bookFound = bookService.getBookDTOById(anyLong());
+        assertEquals(bookFound.getId(), book.getId());
     }
 
     @Test
-    void getBookByTipology() {
+    void getBookById_WhenIdIsNotPresent_ShouldThrowException() {
+        when(bookRepository.findById(book.getId())).thenReturn(Optional.empty());
+        assertThrows(Exception.class, () -> bookService.getBookById(book.getId()));
+        Exception e = assertThrows(Exception.class, () -> bookService.getBookById(anyLong()));
+        assertEquals("Book not found", e.getMessage());
     }
 
     @Test
-    void addBookWithAuthor() {
+    void getBookById_WhenItsAllOk() throws Exception {
+        when(bookRepository.findById(anyLong())).thenReturn(Optional.of(book));
+        Book bookFound = bookService.getBookById(anyLong());
+        assertEquals(bookFound.getId(), book.getId());
     }
 
     @Test
-    void getBookByIdAuthor() {
+    void getBookByTipology_WhenItsAllOk() {
+        when(bookRepository.findByTipologia(GenreBook.FANTASIA)).thenReturn(books);
+        List<BookDTO> fantasyBooks = bookService.getBookByTipology(GenreBook.FANTASIA);
+        assertEquals(fantasyBooks.size(), 2);
     }
 
     @Test
-    void deleteBookByIdAuthor() {
+    void addBookWithAuthor_WhenAuthorNotExist_ShouldThrowException() throws Exception {
+        when(authorService.getAuthorDTOById(anyLong())).thenReturn(null);
+        assertThrows(Exception.class, () -> bookService.addBookWithAuthor(createBookDTO, anyLong()));
     }
 
     @Test
-    void changeTitleToBookDTO() {
+    void addBookWithAuthor_WhenItsAllOk() throws Exception {
+        when(authorService.getAuthorDTOById(1L)).thenReturn(authorDTO);
+        when(authorMapper.fromDTOtoAuthor(authorDTO)).thenReturn(author);
+        when(bookRepository.existsByAuthor_IdAndTitolo(1L, "IT")).thenReturn(false);
+        when(bookMapper.fromDTOToBook(createBookDTO2, author)).thenReturn(book);
+        when(bookRepository.save(book)).thenReturn(book);
+        when(bookMapper.fromBookToDTO(book)).thenReturn(bookDTO);
+        BookDTO addedBook = bookService.addBookWithAuthor(createBookDTO2, 1L);
+        assertEquals(addedBook.getAuthorId(), 1L);
+    }
+
+    @Test
+    void getBookByIdAuthor_WhenItsAllOk() throws Exception {
+        when(bookRepository.findByAuthorId(anyLong())).thenReturn(books);
+        when(bookMapper.fromBookToDTO(book)).thenReturn(bookDTO);
+        when(bookMapper.fromBookToDTO(book2)).thenReturn(bookDTO2);
+
+        List<BookDTO> booksByAuthor = bookService.getBookByIdAuthor(anyLong());
+        assertEquals(booksByAuthor.size(), 2);
+    }
+
+    @Test
+    void getBookByIdAuthor_WhenListIsEmpty_ShouldThrowException() {
+        when(bookRepository.findByAuthorId(anyLong())).thenReturn(List.of());
+        assertThrows(Exception.class, () -> bookService.getBookByIdAuthor(anyLong()));
+    }
+
+    @Test
+    void deleteBookByIdAuthor() throws Exception {
+        when(bookRepository.findByAuthorId(anyLong())).thenReturn(books);
+        boolean result = bookService.deleteBookByIdAuthor(anyLong());
+        assertTrue(result);
+    }
+
+    @Test
+    void changeTitleToBookDTO() throws Exception {
+        when(bookRepository.findByAuthorIdAndId(anyLong(), anyLong())).thenReturn(book);
+        when(bookRepository.save(book)).thenReturn(changedBook);
+        when(bookMapper.fromBookToDTO(book)).thenReturn(changedBookDTO);
+        BookDTO modifyBook = bookService.changeTitleToBookDTO(1L, modifyBookDTO);
+        assertEquals("Il nome della rosa", modifyBook.getTitolo());
+    }
+
+    @Test
+    void changeTitleToBookDTO_WhenBookIsNull_ShouldThrowException() {
+        when(bookRepository.findByAuthorIdAndId(anyLong(), anyLong())).thenReturn(null);
+        assertThrows(Exception.class, () -> bookService.changeTitleToBookDTO(1L, modifyBookDTO));
     }
 }
