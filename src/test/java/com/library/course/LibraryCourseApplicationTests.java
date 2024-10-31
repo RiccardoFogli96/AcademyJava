b7d5dbbc07eda96f9d467926a1b8477a6d304503
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-31 17:48:40 +0100

    Aggiunti test per AuthorController

--- src/test/java/com/library/course/LibraryCourseApplicationTests.java
+++ src/test/java/com/library/course/LibraryCourseApplicationTests.java
@@ -1,10 +1,15 @@
 package com.library.course;
 
+import com.library.course.controller.AuthorController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LibraryCourseApplicationTests {
+
+	@Autowired
+	private AuthorController authorController;
 
 	@Test
 	void contextLoads() {
