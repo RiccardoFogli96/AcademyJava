975547067c2d6fa4b22d205bf61827f14bd8795b
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-31 17:47:19 +0100

    Aggiunto spazio in messaggio eccezione

--- src/main/java/com/library/course/service/AuthorService.java
+++ src/main/java/com/library/course/service/AuthorService.java
@@ -27,7 +27,7 @@ public class AuthorService {
 
     public AuthorDTO getAuthorDTOById( Long id)throws Exception{
         Author author = authorRepository.findById(id)
-                .orElseThrow(() -> new Exception("Author with Id" + id + " not found"));
+                .orElseThrow(() -> new Exception("Author with Id " + id + " not found"));
         return authorMapper.fromAuthorToDTO(author);
     }
 
