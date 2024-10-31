f166951ca2aa589ec640f536f0b9802304fa957a
Luigi Monteforte <monteforteluigi28@gmail.com>
2024-10-31 10:46:54 +0100

    Aggiunta annotazione @Repository

--- src/main/java/com/library/course/repository/CustomerRepository.java
+++ src/main/java/com/library/course/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.library.course.repository;
 
 import com.library.course.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository< Customer, Long > {
     Customer findByEmailAndPassword(String email, String password);
     Customer findByEmail(String email);
