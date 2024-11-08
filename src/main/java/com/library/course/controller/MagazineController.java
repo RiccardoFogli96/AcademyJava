47636005ff6abc47f3ce3e79b280a402c2a587bf
Maria Eduarda Barone <duda.barone@outlook.com>
2024-11-08 13:50:05 +0100

    - Magazine test

--- src/main/java/com/library/course/controller/MagazineController.java
+++ src/main/java/com/library/course/controller/MagazineController.java
@@ -5,11 +5,9 @@ import com.library.course.model.CreateMagazineDTO;
 import com.library.course.model.MagazineDTO;
 import com.library.course.service.MagazineService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
